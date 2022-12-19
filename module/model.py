d39b974ff6676835c26f8866f98798bb02834bd7
moon <d.moon23k@gmail.com>
2022-12-19 20:29:15 +0900

    code updates

---
+++ module/model.py
@@ -0,0 +1,55 @@
+import torch, os
+import torch.nn as nn
+from model.base import Attention
+from model.hier import HierAttention
+
+
+
+def init_normal(model):
+    for name, param in model.named_parameters():
+        if 'weight' in name:
+            nn.init.normal_(param.data, mean=0, std=0.01)
+        else:
+            nn.init.constant_(param.data, 0)
+
+
+def count_params(model):
+    params = sum(p.numel() for p in model.parameters() if p.requires_grad)
+    return params
+    
+
+
+def check_size(model):
+    param_size, buffer_size = 0, 0
+
+    for param in model.parameters():
+        param_size += param.nelement() * param.element_size()
+    
+    for buffer in model.buffers():
+        buffer_size += buffer.nelement() * buffer.element_size()
+
+    size_all_mb = (param_size + buffer_size) / 1024**2
+    return size_all_mb
+
+
+
+def load_model(config):
+    if config.task == 'sum':
+        model = HierModel(config)
+    else:
+        model = BaseModel(config)
+    
+    model.apply(init_uniform)
+    print(f"Initialized model for {config.task} task has loaded")
+
+    if config.mode != 'train':
+        assert os.path.exists(config.ckpt)
+        model_state = torch.load(config.ckpt, map_location=config.device)['model_state_dict']
+        model.load_state_dict(model_state)
+        model.eval()
+        print(f"Model states has loaded from {config.ckpt}")       
+    
+    print(f"--- Model Params: {count_params(model):,}")
+    print(f"--- Model  Size : {check_size(model):.3f} MB\n")
+
+    return model.to(config.device)
